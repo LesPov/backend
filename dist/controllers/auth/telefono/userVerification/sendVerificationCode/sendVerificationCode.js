9f1ece64426332b473d5048e8d73b7772e0e5bbf
LesPo <139312751+LeonardoPove@users.noreply.github.com>
2024-03-02 01:05:15 -0500

    se agrego la funcion para validar si el usario ya estaba valdiado en verificado y en correo_verifcado y ya fucniona perfecto la validacion

--- dist/controllers/auth/telefono/userVerification/sendVerificationCode/sendVerificationCode.js
+++ dist/controllers/auth/telefono/userVerification/sendVerificationCode/sendVerificationCode.js
@@ -12,11 +12,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.sendVerificationCode = exports.findUserByUsernamePhoneSend = void 0;
+exports.handleServerErrorPhoneSend = exports.sendVerificationCode = void 0;
 const errorMesages_1 = require("../../../../../middleware/errorMesages");
 const usuariosModel_1 = __importDefault(require("../../../../../models/usuarios/usuariosModel"));
-const userVerification_1 = require("../../../../../utils/email/userVerification/verifiedUser/user&codeVerification/userVerification");
 const validationUtils_1 = require("../../../../../utils/singup/validation/validationUtils");
+const verificationsModel_1 = __importDefault(require("../../../../../models/verificaciones/verificationsModel"));
 const validateVerificationFieldsPhoneSend = (usuario, celular) => {
     const errors = [];
     if (!usuario || !celular) {
@@ -26,13 +26,20 @@ const validateVerificationFieldsPhoneSend = (usuario, celular) => {
 };
 // Busca un usuario por nombre de usuario, incluyendo su información de verificación
 const findUserByUsernamePhoneSend = (usuario, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const user = yield usuariosModel_1.default.findOne({ where: { usuario: usuario } });
+    const user = yield usuariosModel_1.default.findOne({ where: { usuario: usuario }, include: [verificationsModel_1.default] });
     if (!user) {
         return res.status(400).json({ msg: errorMesages_1.errorMessages.userNotExists(usuario) });
     }
     return user;
 });
-exports.findUserByUsernamePhoneSend = findUserByUsernamePhoneSend;
+const checkUserVerificationStatusPhoneSend = (user) => {
+    if (isUserAlreadyVerifiedPhoneSend(user)) {
+        throw new Error(errorMesages_1.errorMessages.userAlreadyVerified);
+    }
+};
+const isUserAlreadyVerifiedPhoneSend = (user) => {
+    return user.verificacion.verificado || user.verificacion.correo_verificado;
+};
 const sendVerificationCode = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const { usuario, celular } = req.body;
@@ -40,10 +47,26 @@ const sendVerificationCode = (req, res) => __awaiter(void 0, void 0, void 0, fun
         const validationErrors = validateVerificationFieldsPhoneSend(usuario, celular);
         (0, validationUtils_1.handleInputValidationErrors)(validationErrors, res);
         // Buscar al usuario por nombre de usuario
-        const user = yield (0, exports.findUserByUsernamePhoneSend)(usuario, res);
-        (0, userVerification_1.checkUserVerificationStatus)(user);
+        const user = yield findUserByUsernamePhoneSend(usuario, res);
+        checkUserVerificationStatusPhoneSend(user);
     }
-    catch (_a) {
+    catch (error) {
+        (0, exports.handleServerErrorPhoneSend)(error, res);
     }
 });
 exports.sendVerificationCode = sendVerificationCode;
+/**
+ * Maneja errores internos del servidor.
+ * @param error El error ocurrido.
+ * @param res La respuesta HTTP saliente.
+ */
+const handleServerErrorPhoneSend = (error, res) => {
+    console.error("Error en el controlador phonesend:", error);
+    if (!res.headersSent) {
+        res.status(400).json({
+            msg: error.message || errorMesages_1.errorMessages.databaseError,
+            error,
+        });
+    }
+};
+exports.handleServerErrorPhoneSend = handleServerErrorPhoneSend;
--- src/controllers/auth/telefono/userVerification/sendVerificationCode/sendVerificationCode.ts
+++ src/controllers/auth/telefono/userVerification/sendVerificationCode/sendVerificationCode.ts
@@ -1,8 +1,8 @@
 import { errorMessages } from "../../../../../middleware/errorMesages";
 import Usuario from "../../../../../models/usuarios/usuariosModel";
 import { Request, Response } from 'express';
-import { checkUserVerificationStatus } from "../../../../../utils/email/userVerification/verifiedUser/user&codeVerification/userVerification";
 import { handleInputValidationErrors } from "../../../../../utils/singup/validation/validationUtils";
+import Verificacion from "../../../../../models/verificaciones/verificationsModel";
 
 
 const validateVerificationFieldsPhoneSend = (usuario: string, celular: string): string[] => {
@@ -13,16 +13,24 @@ const validateVerificationFieldsPhoneSend = (usuario: string, celular: string): 
     return errors;
 };
 // Busca un usuario por nombre de usuario, incluyendo su información de verificación
-export const findUserByUsernamePhoneSend = async (usuario: string, res: Response) => {
-    const user = await Usuario.findOne({ where: { usuario: usuario } });
+const findUserByUsernamePhoneSend = async (usuario: string, res: Response) => {
+    const user = await Usuario.findOne({ where: { usuario: usuario }, include: [Verificacion] });
 
     if (!user) {
         return res.status(400).json({ msg: errorMessages.userNotExists(usuario) });
     }
     return user;
 };
+const checkUserVerificationStatusPhoneSend = (user: any) => {
+    if (isUserAlreadyVerifiedPhoneSend(user)) {
+        throw new Error(errorMessages.userAlreadyVerified);
+    }
 
+};
 
+const isUserAlreadyVerifiedPhoneSend = (user: any) => {
+    return user.verificacion.verificado || user.verificacion.correo_verificado;
+};
 
 export const sendVerificationCode = async (req: Request, res: Response) => {
     try {
@@ -30,12 +38,29 @@ export const sendVerificationCode = async (req: Request, res: Response) => {
         // Validar campos
         const validationErrors = validateVerificationFieldsPhoneSend(usuario, celular);
         handleInputValidationErrors(validationErrors, res);
-       
+
         // Buscar al usuario por nombre de usuario
         const user = await findUserByUsernamePhoneSend(usuario, res);
-        checkUserVerificationStatus(user);
 
-    } catch {
+        checkUserVerificationStatusPhoneSend(user);
+
+    } catch (error: any) {
+        handleServerErrorPhoneSend(error, res);
 
     }
 }
+
+/** 
+ * Maneja errores internos del servidor.
+ * @param error El error ocurrido.
+ * @param res La respuesta HTTP saliente.
+ */
+export const handleServerErrorPhoneSend = (error: any, res: Response) => {
+    console.error("Error en el controlador phonesend:", error);
+    if (!res.headersSent) {
+        res.status(400).json({
+            msg: error.message ||  errorMessages.databaseError,
+            error,
+        });
+    }
+};
