ba27217c7f4a6587fccb340f05a7bcfe5e3b9563
LesPo <139312751+LeonardoPove@users.noreply.github.com>
2024-03-09 23:33:33 -0500

    Documentacion de validaterole

--- dist/middleware/validateRole/validateRole.js
+++ dist/middleware/validateRole/validateRole.js
@@ -5,41 +5,53 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const errorMessages_1 = require("../errorMessages");
+// Middleware para validar el rol del usuario
 const validateRole = (requiredRole) => {
     return (req, res, next) => {
         try {
+            // Extracción del token de autorización desde la solicitud
             const token = extractToken(req);
+            // Obtención del rol del usuario desde el token
             const userRole = getUserRoleFromToken(token);
+            // Validación del rol del usuario con el rol requerido
             validateUserRole(userRole, requiredRole, res, next);
         }
         catch (error) {
+            // Manejo de errores relacionados con la verificación del token
             return res.status(401).json({
                 msg: errorMessages_1.errorMessages.invalidToken,
             });
         }
     };
 };
+// Función para extraer el token de autorización de la solicitud
 const extractToken = (req) => {
     var _a;
     const token = (_a = req.headers['authorization']) === null || _a === void 0 ? void 0 : _a.split(' ')[1];
+    // Verificación de la presencia del token en el encabezado
     if (!token) {
         throw new Error(errorMessages_1.errorMessages.tokenNotProvided);
     }
     return token;
 };
+// Función para obtener el rol del usuario desde el token
 const getUserRoleFromToken = (token) => {
     const decodedToken = jsonwebtoken_1.default.verify(token, process.env.SECRET_KEY || 'pepito123');
+    // Obtención del rol desde el token decodificado
     return decodedToken.rol;
 };
+// Función para validar el rol del usuario con el rol requerido
 const validateUserRole = (userRole, requiredRole, res, next) => {
     if (userRole === requiredRole || userRole === 'admin') {
         // Si el rol es válido, se permite el acceso a la ruta protegida
         next();
     }
     else {
+        // Si el rol no es válido, se devuelve un código de acceso denegado
         return res.status(403).json({
             msg: errorMessages_1.errorMessages.accessDenied,
         });
     }
 };
+// Exportar el middleware para su uso en otras partes de la aplicación
 exports.default = validateRole;
--- src/middleware/validateRole/validateRole.ts
+++ src/middleware/validateRole/validateRole.ts
@@ -1,15 +1,22 @@
+// Importación de módulos necesarios desde Express y JSON Web Token (JWT)
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import { errorMessages } from '../errorMessages';
 
+// Middleware para validar el rol del usuario
 const validateRole = (requiredRole: string) => {
     return (req: Request, res: Response, next: NextFunction) => {
         try {
+            // Extracción del token de autorización desde la solicitud
             const token = extractToken(req);
+
+            // Obtención del rol del usuario desde el token
             const userRole = getUserRoleFromToken(token);
 
+            // Validación del rol del usuario con el rol requerido
             validateUserRole(userRole, requiredRole, res, next);
         } catch (error) {
+            // Manejo de errores relacionados con la verificación del token
             return res.status(401).json({
                 msg: errorMessages.invalidToken,
             });
@@ -17,9 +24,11 @@ const validateRole = (requiredRole: string) => {
     };
 };
 
+// Función para extraer el token de autorización de la solicitud
 const extractToken = (req: Request): string => {
     const token = req.headers['authorization']?.split(' ')[1];
 
+    // Verificación de la presencia del token en el encabezado
     if (!token) {
         throw new Error(errorMessages.tokenNotProvided);
     }
@@ -27,20 +36,26 @@ const extractToken = (req: Request): string => {
     return token;
 };
 
+// Función para obtener el rol del usuario desde el token
 const getUserRoleFromToken = (token: string): string => {
     const decodedToken: any = jwt.verify(token, process.env.SECRET_KEY || 'pepito123');
+    
+    // Obtención del rol desde el token decodificado
     return decodedToken.rol;
 };
 
+// Función para validar el rol del usuario con el rol requerido
 const validateUserRole = (userRole: string, requiredRole: string, res: Response, next: NextFunction) => {
     if (userRole === requiredRole || userRole === 'admin') {
         // Si el rol es válido, se permite el acceso a la ruta protegida
         next();
     } else {
+        // Si el rol no es válido, se devuelve un código de acceso denegado
         return res.status(403).json({
             msg: errorMessages.accessDenied,
         });
     }
 };
 
+// Exportar el middleware para su uso en otras partes de la aplicación
 export default validateRole;
