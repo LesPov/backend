1988213448a8f955a800d5cf9ac388bb4c008593
LesPo <139312751+LeonardoPove@users.noreply.github.com>
2024-03-09 23:25:07 -0500

    Documentacion validateTOKEN

--- dist/middleware/validateToken/validateToken.js
+++ dist/middleware/validateToken/validateToken.js
@@ -5,36 +5,50 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const errorMessages_1 = require("../errorMessages");
+// Middleware para validar el token de autenticación
 const validateToken = (req, res, next) => {
+    // Obtención del token del encabezado de la solicitud
     const headerToken = req.headers['authorization'];
     try {
+        // Verificación de la presencia y formato correcto del token en el encabezado
         checkTokenPresenceAndFormat(headerToken);
+        // Extracción del token Bearer
         const bearerToken = extractBearerToken(headerToken);
+        // Verificación del token y obtención de la información del usuario
         const decodedToken = verifyTokenAndGetUser(bearerToken);
+        // Adjuntar la información del usuario a la solicitud
         attachUserInfoToRequest(req, decodedToken);
+        // Continuar con el siguiente middleware o ruta
         next();
     }
     catch (error) {
+        // Manejo de errores durante la verificación del token
         handleTokenVerificationError(error, res);
     }
 };
+// Función para verificar la presencia y formato correcto del token
 const checkTokenPresenceAndFormat = (headerToken) => {
     if (!headerToken || !headerToken.startsWith('Bearer ')) {
         throw new Error(errorMessages_1.errorMessages.accessDeniedNoToken);
     }
 };
+// Función para extraer el token Bearer del encabezado
 const extractBearerToken = (headerToken) => {
     return headerToken.slice(7);
 };
+// Función para verificar el token y obtener la información del usuario
 const verifyTokenAndGetUser = (bearerToken) => {
     return jsonwebtoken_1.default.verify(bearerToken, process.env.SECRET_KEY || 'pepito123');
 };
+// Función para adjuntar la información del usuario a la solicitud
 const attachUserInfoToRequest = (req, decodedToken) => {
     req.user = decodedToken;
 };
+// Función para manejar errores durante la verificación del token
 const handleTokenVerificationError = (error, res) => {
     res.status(401).json({
         msg: errorMessages_1.errorMessages.invalidToken,
     });
 };
+// Exportar el middleware para su uso en otras partes de la aplicación
 exports.default = validateToken;
--- src/middleware/validateToken/validateToken.ts
+++ src/middleware/validateToken/validateToken.ts
@@ -1,7 +1,9 @@
+// Importación de módulos necesarios desde Express y JSON Web Token (JWT)
 import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 import { errorMessages } from '../errorMessages';
 
+// Definición de la interfaz CustomRequest que extiende la interfaz Request de Express
 interface CustomRequest extends Request {
     user?: {
         userId: number;
@@ -10,43 +12,60 @@ interface CustomRequest extends Request {
     };
 }
 
+// Middleware para validar el token de autenticación
 const validateToken = (req: CustomRequest, res: Response, next: NextFunction) => {
+    // Obtención del token del encabezado de la solicitud
     const headerToken = req.headers['authorization'];
 
     try {
+        // Verificación de la presencia y formato correcto del token en el encabezado
         checkTokenPresenceAndFormat(headerToken);
+        
+        // Extracción del token Bearer
         const bearerToken = extractBearerToken(headerToken as string);
+
+        // Verificación del token y obtención de la información del usuario
         const decodedToken = verifyTokenAndGetUser(bearerToken);
 
+        // Adjuntar la información del usuario a la solicitud
         attachUserInfoToRequest(req, decodedToken);
+
+        // Continuar con el siguiente middleware o ruta
         next();
     } catch (error) {
+        // Manejo de errores durante la verificación del token
         handleTokenVerificationError(error, res);
     }
 };
 
+// Función para verificar la presencia y formato correcto del token
 const checkTokenPresenceAndFormat = (headerToken: string | undefined) => {
     if (!headerToken || !headerToken.startsWith('Bearer ')) {
         throw new Error(errorMessages.accessDeniedNoToken);
     }
 };
 
+// Función para extraer el token Bearer del encabezado
 const extractBearerToken = (headerToken: string) => {
     return headerToken.slice(7);
 };
 
+// Función para verificar el token y obtener la información del usuario
 const verifyTokenAndGetUser = (bearerToken: string): any => {
     return jwt.verify(bearerToken, process.env.SECRET_KEY || 'pepito123');
 };
 
+// Función para adjuntar la información del usuario a la solicitud
 const attachUserInfoToRequest = (req: CustomRequest, decodedToken: any) => {
     req.user = decodedToken;
 };
 
+// Función para manejar errores durante la verificación del token
 const handleTokenVerificationError = (error: any, res: Response) => {
     res.status(401).json({
         msg: errorMessages.invalidToken,
     });
 };
 
+// Exportar el middleware para su uso en otras partes de la aplicación
 export default validateToken;
