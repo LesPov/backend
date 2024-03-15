2de21ab1b49013c4e2270feca308da0927c33a18
LesPo <139312751+LeonardoPove@users.noreply.github.com>
2024-03-14 22:42:41 -0500

    se modifico el controaldor de recuperacion se le quito el bloqueo de usario cuando pone mal la contraseña aleatorea ,  modifcar el bloqueo listo

--- src/controllers/auth/acceso/passwordRest/passwordResetController/passwordResetController.ts
+++ src/controllers/auth/acceso/passwordRest/passwordResetController/passwordResetController.ts
@@ -18,6 +18,7 @@ const PASSWORD_REGEX_LOWERCASE = /[a-z]/;
 const PASSWORD_REGEX_SPECIAL = /[&$@_/-]/;
 const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
+/////////////////////////////////////////
 
 /**
  * Validar campos requeridos para el envío de .
@@ -37,90 +38,46 @@ export const validateVerificationFieldsResetPass = (usernameOrEmail: string, con
 
     return errors;
 };
-const validateRandomPassword = async (verificacion: VerificacionModel | null, res: Response, contrasena_aleatoria: string): Promise<boolean> => {
 
-    // Verifica si el objeto de verificación o la contraseña aleatoria son nulos
-    if (!verificacion || !contrasena_aleatoria) {
+
+/**
+ * Valida la contraseña aleatoria proporcionada.
+ * @param verification - Objeto de modelo de verificación.
+ * @param res - Objeto de respuesta.
+ * @param randomPassword - Contraseña aleatoria proporcionada.
+ * @returns {boolean} - True si la contraseña aleatoria es válida, false de lo contrario.
+ */
+const validateRandomPassword = (verificacion: VerificacionModel | null, res: Response, contrasena_aleatoria: string): boolean => {
+    if (!verificacion || !contrasena_aleatoria || contrasena_aleatoria.length !== 8) {
         res.status(400).json({
             msg: errorMessages.invalidPassword,
         });
         return false;
     }
-    // Verifica la longitud de la contraseña aleatoria
-    if (contrasena_aleatoria.length !== 8) {
-        // Incrementa el contador de intentos fallidos y maneja el bloqueo de la cuenta si es necesario
-        await incrementFailedAttempts(verificacion);
-        if (verificacion.intentos_ingreso >= MAX_LOGIN_ATTEMPTS) {
-            await lockAccount(verificacion.Usuario);
-            res.status(400).json({
-                msg: errorMessages.accountLocked,
-            });
-            return false;
-        }
-        res.status(400).json({
-            msg: errorMessages.invalidPasswordLength,
-            intentos: verificacion.intentos_ingreso,
-        });
-        return false;
-    }
-    // Verifica si la contraseña aleatoria coincide con la almacenada en el objeto de verificación
+
+    // Verificar si la contraseña aleatoria es la misma que la almacenada en la base de datos
     if (verificacion.contrasena_aleatoria !== contrasena_aleatoria) {
-        // Incrementa el contador de intentos fallidos y maneja el bloqueo de la cuenta si es necesario
-        await incrementFailedAttempts(verificacion);
-        if (verificacion.intentos_ingreso >= MAX_LOGIN_ATTEMPTS) {
-            await lockAccount(verificacion.Usuario);
-            res.status(400).json({
-                msg: errorMessages.accountLocked,
-            });
-            return false;
-        }
         res.status(400).json({
             msg: errorMessages.invalidPasswordDB,
-            intentos: verificacion.intentos_ingreso,
         });
         return false;
     }
-    // Verifica si el código de verificación ha expirado
+
+    // Verificar si la contraseña aleatoria ha expirado
     if (isVerificationCodeExpired(verificacion.expiracion_codigo_verificacion)) {
         res.status(400).json({
             msg: errorMessages.verificationCodeExpired,
         });
         return false;
     }
-  
-    // La contraseña aleatoria ha pasado todas las validaciones
+
+    // Verificar criterios adicionales si es necesario (e.g., uppercase, lowercase, numbers, special characters)
+
     return true;
 };
-/**
- * Incrementa el contador de intentos fallidos e actualiza la fecha de expiración.
- * @param verification - Objeto de modelo de verificación.
- */
-const incrementFailedAttempts = async (verification: VerificacionModel): Promise<void> => {
-    if (verification.intentos_ingreso < MAX_LOGIN_ATTEMPTS) {
-        verification.intentos_ingreso += 1;
-    }
-    verification.expiracion_intentos_ingreso = calculateLockoutExpiration();
-    await verification.save();
-};
-/**
- * Calcula la fecha de expiración para el bloqueo de la cuenta.
- * @returns Fecha de expiración para el bloqueo de la cuenta.
- */
-const calculateLockoutExpiration = (): Date => {
-    const lockoutDurationMinutes = 2; // Cambiar según tus requisitos
-    const currentDateTime = new Date();
-    currentDateTime.setMinutes(currentDateTime.getMinutes() + lockoutDurationMinutes);
-    return currentDateTime;
-};
-/**
- * Verifica si la cuenta está bloqueada.
- * @param verification - Objeto de modelo de verificación.
- * @returns True si la cuenta está bloqueada, false de lo contrario.
- */
-const isAccountLockedOut = (verification: VerificacionModel): boolean => {
-    const currentDateTime = new Date();
-    return verification.intentos_ingreso >= 5 && verification.expiracion_intentos_ingreso > currentDateTime;
-};
+
+
+
 /**
  * Valida si la contraseña aleatoria ha expirado.
  * @param expirationDate - Fecha de expiración almacenada en el registro de verificación.
@@ -130,6 +87,8 @@ const isVerificationCodeExpired = (expirationDate: Date): boolean => {
     const currentDateTime = new Date();
     return currentDateTime > expirationDate;
 };
+
+
 /////////////////////////////////////////
 /**
  * Valida la longitud mínima de la contraseña.
@@ -220,6 +179,7 @@ const validateRandomPasswordAndNewPassword = (verificacion: VerificacionModel | 
         return;
     }
 };
+
 /////////////////////////////////////////////////////
 /**
  * Actualiza y borra la contraseña del usuario.
@@ -233,7 +193,6 @@ const updateAndClearPassword = async (user: UsuarioModel, verificacion: Verifica
     if (verificacion) {
         verificacion.contrasena_aleatoria = '';
         verificacion.expiracion_codigo_verificacion = new Date();
-        verificacion.intentos_ingreso = 0; // Reiniciar el contador de intentos fallidos
         await verificacion.save();
     }
     await user.save();
@@ -249,20 +208,16 @@ export const passwordresetPass = async (req: Request, res: Response) => {
             contrasena_aleatoria,
             newPassword
         );
+        handleInputValidationErrors(inputValidationErrors, res);
         // Buscar al usuario por nombre de usuario
         const user = await findUserByUsernameRecoveryPass(usernameOrEmail, res);
         // Verificar la propiedad de verificación del usuario
         checkUserVerificationStatusLogin(user, res);
-        // Verificar si el usuario ha excedido el número máximo de intentos de inicio de sesión y manejar el bloqueo de la cuenta
-        await checkLoginAttemptsAndBlockAccount(user, res);
         // Buscar o crear un registro de verificación para el usuario
         const verification = await findOrCreateVerificationRecoveryPass(user.usuario_id);
         // Validar la contraseña aleatoria y si ya expiración 
-        const isRandomPasswordValid = await validateRandomPassword(verification, res, contrasena_aleatoria);
-        if (!isRandomPasswordValid) {
-            return; // ¡Importante! Salir de la función después de enviar la respuesta
-        }
-        // Validar la nueva contraseña
+        validateRandomPassword(verification, res, contrasena_aleatoria);
+        // Validar la nueva contraseñ
         validateRandomPasswordAndNewPassword(verification, res, contrasena_aleatoria, newPassword);
         // Actualizar y borrar la contraseña del usuario
         await updateAndClearPassword(user, verification, newPassword);
